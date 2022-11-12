267ea5134f5232aaecc0c4ce582370dc4b953a6d
Stefan Dukic <sdukic@1-D-23.42heilbronn.de>
2022-11-12 21:51:19 +0100

    sending with interrupt works

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sdukic <sdukic@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:39:39 by sdukic            #+#    #+#             */
-/*   Updated: 2022/11/10 18:12:30 by sdukic           ###   ########.fr       */
+/*   Updated: 2022/11/12 21:48:46 by sdukic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,47 @@
 #include "libft/libft.h"
 #include <signal.h>
 
+int	g_ack;
+
+void	handle_sig(int signal, siginfo_t *info, void *context)
+{
+	(void)(info);
+	(void)(context);
+	if (signal == SIGUSR1)
+	{
+		g_ack = 1;
+	}
+}
+
+void	send_message(int serverid, char *message)
+{
+	int				sig;
+	unsigned int	shift;
+	struct sigaction sa = { 0 };
+
+	sa.sa_sigaction = &handle_sig;
+	while (!g_ack)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		kill(serverid, SIGUSR2);
+		usleep(200);
+	}
+	while (*message)
+	{
+		shift = 8;
+		while (shift)
+		{
+			sig = (*message & (1 << --shift)) != 0;
+			kill(serverid, sig ? SIGUSR1 : SIGUSR2);
+			usleep(200);
+		}
+		message++;
+	}
+}
+
 int	main(int argc, char *argv[])
 {
-	char	*serverid;
+	int		serverid;
 	char	*message;
 
 	if (argc != 3)
@@ -24,9 +62,8 @@ int	main(int argc, char *argv[])
 		ft_printf("usage: client [serverid] [messsage]\n");
 		return (0);
 	}
-	serverid = argv[1];
+	serverid = ft_atoi((const char*) argv[1]);
 	message = argv[2];
-	ft_printf("ServerID: %s\n", serverid);
-	ft_printf("message: %s\n", message);
+	send_message(serverid, message);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,23 +6,66 @@
 /*   By: sdukic <sdukic@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 16:52:00 by sdukic            #+#    #+#             */
-/*   Updated: 2022/11/10 18:39:26 by sdukic           ###   ########.fr       */
+/*   Updated: 2022/11/12 21:21:19 by sdukic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minitalk.h"
 #include "libft/libft.h"
+#include <signal.h>
 
-void	handle_signal(int signal)
+static volatile int signalPid = -1;
+
+void	handle_bin(int bin)
 {
-	ft_printf("Received Signal\n");
+	static unsigned int	bits;
+	static char			c;
+
+	if (!c)
+		c = 0;
+	c *= 2;
+	if (bin)
+		c += 1;
+	if (bits == 7)
+	{
+		ft_printf("%c", c);
+		c = 0;
+		bits = 0;
+		kill(signalPid, SIGUSR1);
+	}
+	else
+	{
+		bits++;
+	}
+}
+
+void	handle_sig(int signal, siginfo_t *info, void *context)
+{
+	signalPid = info->si_pid;
+	(void)(context);
+
+	if (signal == SIGUSR1)
+	{
+		handle_bin(1);
+	}
+	if (signal == SIGUSR2)
+	{
+		handle_bin(0);
+	}
 }
 
 int	main(void)
 {
+	struct sigaction sa = { 0 };
 	int	pid;
 
 	pid = getpid();
+	sa.sa_sigaction = &handle_sig;
 	ft_printf("ServerID: %d\n", pid);
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+	}
 	return (0);
 }
